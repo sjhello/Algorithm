4569bc8796f0e5362502d82584a9b0df49aa83e1
sjhello <sjhello235@gmail.com>
2022-08-29 17:41:42 +0900

    inflearn: 바둑이 승차 - dfs, 부분집합의 합 문제와 비슷하게 풀이

---
+++ src/main/java/inflearn/Problem8_02.java
@@ -0,0 +1,37 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Problem8_02 {
+    static int answer = 0;
+    static int c;
+    static int n;
+    static int[] weight;
+
+    public void DFS(int level, int sum, int[] arr) {
+        if (sum > c) {
+            return ;
+        }
+
+        if (level == n) {
+            answer = Math.max(answer, sum);
+        } else {
+            DFS(level + 1, arr[level] + sum, arr);
+            DFS(level + 1, sum, arr);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        c = sc.nextInt();   // 목표 무게(넘지않아야함)
+        n = sc.nextInt();   // 바둑이
+        weight = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            weight[i] = sc.nextInt();
+        }
+        Problem8_02 problem8_02 = new Problem8_02();
+        problem8_02.DFS(0, 0, weight);
+        System.out.println(answer);
+    }
+}
