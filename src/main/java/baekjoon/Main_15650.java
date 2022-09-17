dca08ed4472c3048d707db1b285be153a0798e81
sjhello <sjhello235@gmail.com>
2022-09-17 23:10:42 +0900

    baekjoon: 15650, 15651(N과 M(2), N과 M(3)) - 백트래킹(dfs)

---
+++ src/main/java/baekjoon/Main_15650.java
@@ -0,0 +1,46 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main_15650 {
+	static int n;
+	static int m;
+	static int[] numbers;
+	static int[] visited;
+	static int[] answer;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void dfs(int level, int depth) {
+		if (level == m) {
+			for (int number : answer) {
+				sb.append(number).append(" ");
+			}
+			sb.append("\n");
+		} else {
+			for (int i = depth; i < numbers.length; i++) {
+				if (visited[i] == 0) {
+					visited[i] = 1;
+					answer[level] = numbers[i];
+					dfs(level + 1, i + 1);
+					visited[i] = 0;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		numbers = new int[n];
+		visited = new int[n];
+		answer = new int[m];
+
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = i + 1;
+		}
+
+		Main_15650.dfs(0, 0);
+		System.out.println(sb);
+	}
+}
---
+++ src/main/java/baekjoon/Main_15651.java
@@ -0,0 +1,42 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main_15651 {
+	static int n;
+	static int m;
+	static int[] answer;
+	static int[] visited;
+	static int[] numbers;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void dfs(int level) {
+		if (level == m) {
+			for (int number : answer) {
+				sb.append(number).append(" ");
+			}
+			sb.append("\n");
+		} else {
+			for (int i = 0; i < numbers.length; i++) {
+				answer[level] = numbers[i];
+				dfs(level + 1);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		answer = new int[m];
+		visited = new int[n];
+		numbers = new int[n];
+
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = i + 1;
+		}
+
+		Main_15651.dfs(0);
+		System.out.println(sb);
+	}
+}
