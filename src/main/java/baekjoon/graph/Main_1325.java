e1a9e26f9463a871617bab2dc9216ea8f4382ab4
sjhello <sjhello235@gmail.com>
2022-10-09 17:53:47 +0900

    baekjoon: 1325(효율적인 해킹) - 그래프(인접 리스트)

---
+++ src/main/java/baekjoon/graph/Main_1325.java
@@ -0,0 +1,87 @@
+package baekjoon.graph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+
+/*
+* 효율적인 해킹(인접 리스트)
+* */
+public class Main_1325 {
+	static int n;
+	static int m;
+	// static int[][] map;
+	static ArrayList<ArrayList<Integer>> map;
+	static int[] visited;
+	static int[] answer;
+	static int count;
+
+	public static void bfs(int number) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(number);
+		visited[number] = 1;
+
+		while (!queue.isEmpty()) {
+			int current = queue.poll();
+
+			// for (int i = 1; i <= n; i++) {
+			// 	if (map[i][current] == 1 && visited[i] == 0) {
+			// 		visited[i] = 1;
+			// 		count++;
+			// 		queue.add(i);
+			// 	}
+			// }
+
+			for (int next : map.get(current)) {
+				if (visited[next] == 0) {
+					visited[next] = 1;
+					queue.add(next);
+					count++;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		// map = new int[n + 1][n + 1];
+		map = new ArrayList<>();
+		answer = new int[n + 1];
+
+		for (int i = 0; i <= n; i++) {
+			map.add(i, new ArrayList<>());
+		}
+
+		for (int i = 0; i < m; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			// map[a][b] = 1;
+			map.get(b).add(a);
+		}
+
+		printAnswer();
+	}
+
+	private static void printAnswer() {
+		int result = 0;
+
+		for (int i = 1; i <= n; i++) {
+			visited = new int[n + 1];
+			count = 0;
+			bfs(i);
+			answer[i] = count;
+			result = Math.max(result, answer[i]);
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= n; i++) {
+			if (result == answer[i]) sb.append(i + " ");
+		}
+
+		System.out.println(sb);
+	}
+}
