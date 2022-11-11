eb5f00e3c330a86afda2d918f0c911d6b32d676b
sjhello <sjhello235@gmail.com>
2022-11-11 16:48:46 +0900

    feat: 1316 - 문자열, 아스키코드

---
+++ src/main/java/baekjoon/문자열/Main_1316.java
@@ -0,0 +1,46 @@
+package baekjoon.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_1316 {
+
+	static int result;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws IOException {
+		int n = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < n; i++) {
+			if (isGroupSt()) {
+				result++;
+			}
+		}
+
+		System.out.println(result);
+	}
+
+	public static boolean isGroupSt() throws IOException {
+		boolean flag = true;
+		boolean[] strCheckArray = new boolean[26];		// 소문자 알바벳 갯수만큼 배열 생성
+		String str = br.readLine();
+
+		int ch = 0;		// 비교대상 문자 0으로 초기화 해도 되는 이유는 입력이 알파벳 소문자이기 때문이다
+		for (int i = 0; i < str.length(); i++) {
+			int current = str.charAt(i);		// 현재 문자
+
+			if (ch != current) {
+				if (!strCheckArray[current - 97]) {		// 아스키 코드 참고
+					strCheckArray[current - 97] = true;
+					ch = current;
+				} else {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+}
